81eef00fe58cc6a81cbddda6aa92702248a13cc6
osama00001 <osama.sajid@qavitechnologies.com>
2026-06-16 17:16:52 +0500

    build fix

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,26 @@ const nextConfig = {
     "http://localhost:3000",
     "http://192.168.1.10:3000",
   ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.media.strapiapp.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "**.strapiapp.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "1337",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
